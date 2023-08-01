13a4662361c5cc4444ba0e0753cad347d96d42fe
AgielF <agielnanda2004@gmail.com>
2023-08-01 18:47:22 +0700

    commit pertama

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Belajar from "./belajar.svelte";
+  let strHtml = "tag html dari gerak=<marquee>HTML<marquee/>";
+  let count = 0;
+  let arrayFactorial = [1];
+  $: doubleCount = count * 2;
+
+  function pencet() {
+    count += 1;
+    arrayFactorial = [...arrayFactorial, arrayFactorial.length + 1];
+  }
+</script>
+
+<main>
+  {@html strHtml}
+  <div class="card">
+    <Belajar />
+  </div>
+  <button on:click={pencet}>
+    count={count}
+  </button>
+  <p>{doubleCount}</p>
+  <p>{arrayFactorial}</p>
+</main>
---
+++ src/belajar.svelte
@@ -0,0 +1,13 @@
+<script>
+  let src = new URL("./assets/images/gambar.jpg", import.meta.url).href;
+</script>
+
+<img {src} alt="INi gambar" />
+
+<style>
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+  }
+</style>
